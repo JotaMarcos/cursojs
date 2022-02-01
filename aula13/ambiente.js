c1438f506798d966ffd02be132675cfda6f7ee7f
João Marcos Aires Duarte <joao.duarte@ITBAM.ORG.BR>
2022-02-01 07:37:24 -0400

    Prática com Laço de Repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,19 @@
+var contador = 1
+do{
+    console.log(`Passo ${contador}`)
+    contador++
+} while(contador <= 6)
+
+
+/*var contador = 1
+while(contador <= 6){
+    console.log(`Passo ${contador}`)
+    contador++
+}*/
+
+/*console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,18 @@
+/*var contador = 1
+while(contador <= 6){
+    console.log(`Passo ${contador}`)
+    contador++
+}*/
+
+// ----------------------------------------------
+
+// for(var contador = 1; contador <= 6; contador++){
+//     console.log(`Passo ${contador}`)
+// }
+
+
+console.log('Vai começar...')
+for(var c = 1; c <= 6; c++){
+    console.log(c)
+}
+console.log('FIM!')
